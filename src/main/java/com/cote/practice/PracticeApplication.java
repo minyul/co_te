e0bedd8b5aaee8a3a3b190414fdcd6d9033e8ef9
minyul <ggomjae@gmail.com>
2022-03-18 21:45:52 +0900

    docs : 코테 맥북 확인용 ㅎㅅㅎ

--- src/main/java/com/cote/practice/PracticeApplication.java
+++ src/main/java/com/cote/practice/PracticeApplication.java
@@ -3,7 +3,7 @@ package com.cote.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //
 public class PracticeApplication {
 
 	public static void main(String[] args) {
