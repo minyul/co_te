413a5422d087af034869e60fa4e31659e31be038
minyul <ggomjae@gmail.com>
2022-04-16 13:18:48 +0900

    코테 감잡기 어렵눼

--- src/main/java/com/cote/practice/강의/a.java
+++ src/main/java/com/cote/practice/강의/a.java
@@ -1,5 +1,18 @@
 package com.cote.practice.강의;
 
+import java.util.HashMap;
+
 public class a {
 
+	public static void main(String[] args) {
+		String str = "abcdd";
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (Character c : str.toCharArray()) {
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+		for (Character c : map.keySet()) {
+			System.out.println(c);
+			System.out.println(map.get(c));
+		}
+	}
 }
