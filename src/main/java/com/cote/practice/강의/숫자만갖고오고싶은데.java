66eb748f2a2279f0bc421157718fde151e039494
minyul <ggomjae@gmail.com>
2022-04-17 21:46:54 +0900

    feat : 숫자만 갖고오고 싶은데.. toCharArray() , Character.isDigit(c) 으로

--- src/main/java/com/cote/practice/강의/숫자만갖고오고싶은데.java
+++ src/main/java/com/cote/practice/강의/숫자만갖고오고싶은데.java
@@ -1,4 +1,19 @@
 package com.cote.practice.강의;
 
 public class 숫자만갖고오고싶은데 {
+	
+	public static void main(String[] args) {
+		String str = "a0bc1fe3";
+		int answer; // 내가 원하는 답은 13인데.
+
+		String tempStr = "";
+		for (char c : str.toCharArray()) {
+			if (Character.isDigit(c)) {
+				tempStr += c;
+			}
+		}
+
+		answer = Integer.parseInt(tempStr);
+		System.out.println(answer);
+	}
 }
